6673ab4752994e9d0de60ac115014c855c0b722e
GitFranSantos <fjsantosestudios@gmail.com>
2023-10-03 21:13:02 +0200

    Actualizacion

---
+++ Tema1/ejercicioColecciones2/ejercicioColecciones2/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioColecciones2
+{
+    public class Persona
+    {
+        private string nombre;
+        private string longitud;
+
+        public Persona(string nombre)
+        {
+            this.nombre = nombre;
+            this.longitud = nombre.Length;
+        }
+
+        public string Nombre
+        {
+            get => nombre;  
+            set => nombre = value;  
+        }
+
+        public string Longitud
+        {
+            get => longitud;    
+            set => longitud = value;    
+        }
+
+    }
+}
---
+++ ejercicioHerencia/ejercicioHerencia/Coche.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioHerencia
+{
+    public class Coche : Vehiculo //coche hereda de vehiculo
+    {
+        private int velocidad;
+        private int cilindrada;
+
+        public int Cubicaje 
+        {  
+            get => cilindrada; 
+            set => cilindrada = value; 
+        }
+
+        public void setVelicidad(int velocidad)
+        {
+            this.velocidad = velocidad;
+        }
+
+        public int getVelocidad()
+        {
+            return this.velocidad;
+        }
+    }
+}
