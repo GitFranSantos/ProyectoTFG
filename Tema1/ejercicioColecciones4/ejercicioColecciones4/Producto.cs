0879554bc19a54ae8f345317d53b0d8cdc8e0b7b
GitFranSantos <fjsantosestudios@gmail.com>
2023-10-06 08:42:08 +0200

    actualizacion

---
+++ Tema1/ejercicioColecciones4/ejercicioColecciones4/Producto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioColecciones4
+{
+    public class Producto
+    {
+        private int cantidad;
+        private double precio;
+
+        public Producto(int cantidad, double precio)
+        {
+            this.cantidad = cantidad;
+            this.precio = precio;
+        }
+        public int getCantidad()
+        {
+            return cantidad;
+        }
+        public double getPrecio()
+        {
+            return precio;
+        }
+        public double precioFinal()
+        {
+            return Convert.ToDouble(this.precio * this.cantidad);
+        }
+    }
+}
+    
